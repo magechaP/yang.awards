c2224b6e596c4308fd51e5e249e7ddf6130cbf60
magechaP <magechapeter@gmail.com>
2019-09-09 11:07:31 +0300

    create an ev=nvironment file for our app

--- awaards/models.py
+++ awaards/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User
 from tinymce.models import HTMLField
 from url_or_relative_url_field.fields import URLOrRelativeURLField
 
-# Create your models here.
+
 class Profile(models.Model):
     user = models.OneToOneField(User,on_delete=models.CASCADE,null=True,blank=True)
     profile_pic = models.ImageField(default='image.png',upload_to='profiles/')
@@ -32,3 +32,59 @@ class Profile(models.Model):
     
     def __str__(self):
         return self.bio
+class Project(models.Model):
+    profile = models.ForeignKey(User,null=True,on_delete=models.CASCADE) 
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    #technology = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="projects/")
+    # usability=models.IntegerField(default=0)
+    # design=models.IntegerField(default=0)
+
+    # content=models.IntegerField(default=0)
+    link = URLOrRelativeURLField(max_length=200)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    
+    @classmethod
+    def search_by_projects(cls,search_term):
+        projects = cls.objects.filter(title__icontains=search_term)
+        print(projects)
+        return projects 
+    
+    @classmethod
+    def get_profile_projects(cls,profile):
+       projects = Project.objects.filter(profile__pk=profile)
+       print(projects)
+       return projects
+    @classmethod   
+    def search_project(cls, search_term):
+        
+        searched_item=Project.objects.filter(title__icontains=search_term)
+        return searched_item
+        
+    
+    def __str__(self):
+        return self.title
+class Rating(models.Model):
+    design = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    usability = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)])
+    content = models.PositiveIntegerField(default=0,validators=[MaxValueValidator(10)]) 
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+    average_vote=models.FloatField(default=0)
+    project = models.ForeignKey(Project)
+    
+    def save_rating(self):
+       
+        self.save()
+        
+    def delete_rating(self):
+        
+        self.delete()
+        
+    def update_rating(self):
+       
+        self.update()
+        
+   
+    def __str__(self):
+        return self.name
